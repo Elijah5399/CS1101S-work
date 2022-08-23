4d503444a549cf95b954dbecc3f33cde2331f789
Source Academy User <No public email provided>
2022-08-23 21:16:33 +0800

    updated answer

--- S3-extra.js
+++ S3-extra.js
@@ -1,32 +1,48 @@
+function square(x) {
+    return x * x;
+}
+function is_even(n) {
+    return n % 2 === 0;
+}
+
 //n can be all natural numbers
-function fast_expt(b,n) {
-    return n <= 1 ?
-    b :
-    n % 2 === 0 ?
-    fast_expt(b,n/2) * fast_expt(b,n/2):
-    b * fast_expt(b,n-1);
+function fast_expt(b, n) {
+    return n === 0 
+    ? 1
+    : is_even(n)
+      ? square(fast_expt(b, n / 2)) 
+      : b * fast_expt(b, n - 1);
 }
 
 //test fast_expt(2,3);
 
 //extended to all integer powers
-function fast_expt1(b,n) {
-    return math_abs(n) === 1 ?
-    math_pow(b,n) :
-    n % 2 === 0 ?
-    fast_expt1(b,n/2) * fast_expt1(b,n/2):
-    b * fast_expt1(b,n-1);
+function fast_expt1(b, n) {
+    return n < 0 
+    ? 1 / fast_expt1(b, -n)
+    : n === 0
+    ? 1
+    : is_even(n)
+        ? square(fast_expt1(b, n / 2))
+        : b * fast_expt1(b, n - 1);
 }
 
-//test fast_expt1(2,3);
+//test fast_expt1(2,-4);
 //currently using a recursive process.
 //let's try an iterative one
-function expt_iter(b,n,current_value,expo_count) {
-    return expo_count === n + 1 ?
-    current_value:
-    n % 2 === 1 ?
-    expt_iter(b,n-1,b,expo_count+1):
-    expt_iter(b,n,math_pow(b,2),expo_count*2);
+function iter(exp, base, product) {
+    return exp < 0
+    ? iter(-exp, 1 / base, product)
+    : exp === 0 
+    ? product 
+    : exp % 2 === 0
+    ? iter(exp / 2, base * base, product)
+    : iter(exp - 1, base, base * product);
 }
 
-expt_iter(3,3,1,1);
+function fast_iter(b, n) {
+    return iter(n, b, 1);
+}
+
+//test fast_iter(2,-3);
+
